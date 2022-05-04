6b23a6ac9c126216f5059a2ec83c99ac8456fc1f
Camille <folcamille@yahoo.fr>
2022-05-04 15:06:40 +0200

    Initial Commit

---
+++ SurfSpots/SurfSpotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurfSpotsApp.swift
+//  SurfSpots
+//
+//  Created by Camille FOL on 04/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SurfSpotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
